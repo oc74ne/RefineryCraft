ed2a7f74a6dcf1be24ef64c29e2c391538536b68
Chris Nipperess <oc74ne@gmail.com>
2014-06-10 18:06:33 +1000

    main class setup

--- src/main/java/net/oc74ne/refinerycraft/main/RefineryCraft.java
+++ src/main/java/net/oc74ne/refinerycraft/main/RefineryCraft.java
@@ -1,7 +1,35 @@
 package net.oc74ne.refinerycraft.main;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
 /**
  * Created by Chris from 10/06/2014.
  */
+
+@Mod(modid = RefineryCraft.modid, version = RefineryCraft.version, name = "Refinery Craft")
+
 public class RefineryCraft {
+
+    public static final String modid = "refinerycraft";
+    public static final String version = "0.0.1";
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent preEvent){
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent postEvent){
+
+    }
+
 }
